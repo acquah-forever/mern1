5dc02a87f48031c1bc7bd319d6755475a07ea2da
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-02 13:29:16 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+
+
+import { Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className=' bg-linear-to-br from-slate-900 to-slate-400  min-h-screen text-black flex flex-col'>
+      <div className='container mx-auto flex flex-col flex-1'>
+        <NavBar />
+        <main className='flex-1'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
