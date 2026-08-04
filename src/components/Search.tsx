dc10672a2681ff9535312c1555c876bf5d840eed
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-04 15:39:33 +0000

    added edit an delete buttons to thoughts

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { Trash, Pen } from 'lucide-react';
+
 
 const Search = () => {
 
@@ -15,20 +17,28 @@ const Search = () => {
         if (thought === "") {
             return; 
         }
+        if (!thought.startsWith("#")) {
+            alert("Please start your thought with a #");
+            return;
+        }
         setThoughts([...thoughts, thought]);
         setInputValue("");  
     }
   return (
     <div className='flex flex-col items-center justify-center w-full'>
-        <form className="flex  items-center justify-center gap-2 border border-gray-300 rounded-lg px-4 py-2 max-w-4xl w-full">
-            <input  className="py-2 px-4 w-full outline-none border-none focus-visible:ring-2 focus-visible:ring-teal-500" type="text" value={inputValue} placeholder="Whats on your mind?" onChange={handleChange} />
+        <form className="flex  items-center justify-center gap-2 border border-white rounded-lg px-4 py-2 max-w-4xl w-full">
+            <input  className="py-2 px-4 w-full outline-none border-none focus-visible:ring-2 focus-visible:ring-sky-300" type="text" value={inputValue} placeholder="Whats on your mind?(begin thought with #)" onChange={handleChange} />
             <button type='submit' className="bg-teal-400 rounded text-md px-7 py-3"onClick={handleClick}>Send</button>
         </form>
 
         <div className='mt-5 flex flex-col justify-center w-full max-w-4xl'>
             {thoughts.map((thought, index) => (
-                <div key={index} className='bg-white/50 p-3 rounded-lg mb-2'>
-                    <h1>{thought}</h1>
+                <div key={index} className='bg-white/50 p-3 rounded-lg mb-2 flex items-center justify-between'>
+                    <p>{thought}</p>
+                    <div className='flex gap-2'>
+                        <Pen />
+                        <Trash />
+                    </div>
                 </div>
             ))}
         </div>
