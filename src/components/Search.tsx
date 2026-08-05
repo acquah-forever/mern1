851e79e3d675063330717352bd0756f72a73d8da
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-05 20:43:43 +0000

    git commit

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -68,7 +68,7 @@ const Search = () => {
                         </div>
                         {edit?.index === index && (
                             <div className='border-2 border-gray-300 rounded-lg px-7 py-5 w-full'>
-                                <h1 className='italic mb-4'>Edit Thought</h1>
+                                <h1 className='italic mb-4'>Edit Thought Here</h1>
                                 <input className='w-full border-2 border-sky-600 outline-none mb-5 p-3' type="text" onChange={handleChange1} />
                                 <div className='flex justify-end gap-2 mt-2'>
                                     <button className='cursor-pointer bg-teal-400 rounded text-md px-4 py-2'onClick={handleSave}>Save</button>
