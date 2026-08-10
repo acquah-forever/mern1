16d09b64a30523aee9d8418a02ab7fe741e45af4
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-10 13:57:42 +0000

    changed delete to cancel

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -91,7 +91,7 @@ const Search = () => {
                                 <input className='w-full border-2 border-sky-600 outline-none mb-5 p-3 text-sm ms:text-lg' type="text" value={edit.value} onChange={handleChange1} />
                                 <div className='flex justify-end gap-2 mt-2'>
                                     <button className='cursor-pointer bg-teal-400 rounded text-sm sm:text-md px-4 py-2' onClick={handleSave}>Save</button>
-                                    <button className='cursor-pointer bg-red-500 rounded text-sm sm:text-md px-4 py-2' onClick={() => setEdit(null)}>Delete</button>
+                                    <button className='cursor-pointer bg-red-500 rounded text-sm sm:text-md px-4 py-2' onClick={() => setEdit(null)}>Cancel</button>
                                 </div>
                             </div>
                         )}
