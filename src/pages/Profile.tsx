d3337b3380bbc20bb3ecc98b8e64ca4802800b63
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-12 22:50:34 +0000

    finalised profile paghe header

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -8,11 +8,19 @@ const Profile = () => {
         <div className="border rounded-full w-25 h-25 flex justify-center items-center bg-gray-400">
           <UserRoundArrowLeft size={55} />
         </div>
-        <h1 className="text-2xl font-semibold">Username</h1>
+        <div className="flex items-center space-x-5">
+          <h1 className="text-2xl font-semibold">Username</h1>
+          <button className="btn btn-soft btn-primary text-lg">Get Verified</button>
+        </div>
+        <h1>@username</h1>
       </div>
       <div className='flex items-center space-x-3'>
-        <button className="btn btn-primary border border-black border-2">Share Profile</button>
-        <button className="btn btn-info border border-white border-2">Edit Profile</button>
+        <button className="btn btn-primary px-7 py-6 border-black border-2">Share Profile</button>
+        <button className="btn btn-error px-7 py-6 border-white border-2 text-white">Edit Profile</button>
+      </div>
+      <div className="flex items-center mt-10">
+        <div className="grow border-t border-gray-300 border-2"></div>
+        <div className="grow border-t border-gray-300 border-2"></div>
       </div>
     </NavLink>
   )
