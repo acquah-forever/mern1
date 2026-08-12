7dbf824c56de90d44c6d260fb0ace78a1ef31aad
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-12 22:16:31 +0000

    initiated Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,17 @@
-
+import { UserRoundArrowLeft } from "lucide-react"
+import { NavLink } from "react-router-dom"
 
 const Profile = () => {
   return (
-    <div>
-      
-    </div>
+    <NavLink to='/profile'>
+      <div>
+        <UserRoundArrowLeft size={50} />
+        <h1>Username</h1>
+        <button>Share Profile</button>
+        <button>Edit Profile</button>
+      </div>
+
+    </NavLink>
   )
 }
 
