bc336e9507b2b61023068cc72abd366ce994fbff
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-08-12 22:40:11 +0000

    added buttons from daisy ui

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,18 +3,17 @@ import { NavLink } from "react-router-dom"
 
 const Profile = () => {
   return (
-    <NavLink to='/profile' className='grid grid-cols-1'>
-      <div>
+    <NavLink to='/profile'>
+      <div className="mb-7">
         <div className="border rounded-full w-25 h-25 flex justify-center items-center bg-gray-400">
           <UserRoundArrowLeft size={55} />
         </div>
         <h1 className="text-2xl font-semibold">Username</h1>
-        <div>
-          <button>Share Profile</button>
-          <button>Edit Profile</button>
-        </div>
       </div>
-
+      <div className='flex items-center space-x-3'>
+        <button className="btn btn-primary border border-black border-2">Share Profile</button>
+        <button className="btn btn-info border border-white border-2">Edit Profile</button>
+      </div>
     </NavLink>
   )
 }
